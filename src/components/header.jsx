c97f1a9f0450a776afab16d90a6d340356f455b8
shardakumari505 <shardakumari505@gmail.com>
2023-06-19 15:18:20 +0530

    updated navpanel

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,17 +11,16 @@ const Header = () => {
         <div className="header-page-top-container">
 
             <div className="header-page-top-left-navigation-panel">
-                <Navigation />
+                <div className="header-page-top-navigation"><Navigation /></div>
+                <Link className='' to='/'><div className="logo-left"><img className="logo-1" src={Logo} alt="az" /></div></Link>
             </div>
-
-            <Link className='' to='/'><div className="logo-left"><img className="logo-1" src={Logo} alt="az" /></div></Link>
 
             <div className="header-page-top-text">Control Panel</div>
 
             <div className="logo-right"><img className="logo-2" src={Mslogo} alt="microsoft" /></div>
 
             <div className="header-page-top-right">
-                <div className="header-page-top-right-user-icon"><AccountCircleIcon sx={{fontSize: 40}} /></div>
+                <div className="header-page-top-right-user-icon"><AccountCircleIcon sx={{ fontSize: 40 }} /></div>
                 <div className="header-page-top-right-user-options-container">
                     <div className="header-page-top-right-user-option option-1">Account</div>
                     <div className="header-page-top-right-user-option option-2">Settings</div>
--- src/navigation-panel/navigation-panel.jsx
+++ src/navigation-panel/navigation-panel.jsx
@@ -1,55 +1,82 @@
-import React, { useState } from "react";
-import './navigation-panel.scss';
+import * as React from 'react';
 import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import PeopleIcon from '@mui/icons-material/People';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import DeviceHubIcon from '@mui/icons-material/DeviceHub';
 import HubIcon from '@mui/icons-material/Hub';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function Navigation() {
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
 
-    const [navList, setNavlist] = useState(false);
-    const showNavlist = () => setNavlist(!navList);
 
-    return (
-        <div className={navList ? 'navlist active' : 'navlist'}>
-            <div id="hamburger" className="hamburger" type="button" onClick={showNavlist}>
-                {navList? <KeyboardArrowLeftIcon sx={{fontSize: 40, position: 'absolute', top: '20px', right: '20px' }} /> : <MenuIcon sx={{ fontSize: 30,position: 'absolute', right: '-50px'}} />}
-            </div>
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-            <ul id='nav-list' onClick={showNavlist}>
-                <Link className="nav-panel-link" to='/'><li>
-                    <DashboardIcon />
-                    <span class="nav-text">
-                        Dashboard
-                    </span>
-                </li></Link>
+    setState({ ...state, [anchor]: open });
+  };
 
-                <Link className="nav-panel-link" to='/fleet'><li>
-                    <DeviceHubIcon />
-                    <span class="nav-text">
-                        Fleets
-                    </span>
-                </li></Link>
+  const [open, setOpen] = React.useState(false);
 
-                <Link className="nav-panel-link" to='/cluster'><li>
-                    <HubIcon />
-                    <span class="nav-text">
-                        Clusters
-                    </span>
-                </li></Link>
+  const handleItemClick = (link) => {
+    window.location.href = link; // Redirect to the specified link
+    setOpen(false); // Close the drawer
+  };
 
-                <Link className="nav-panel-link" to='/cluster'><li>
-                    <PeopleIcon />
-                    <span class="nav-text">
-                        Users
-                    </span>
-                </li></Link>
-            </ul>
-        </div>
-    );
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+          {[
+            { text: 'Dashboard', icon: <DashboardIcon />, link: '/' },
+            { text: 'Fleets', icon: <DeviceHubIcon />, link: '/fleet' },
+            { text: 'Clusters', icon: <HubIcon />, link: '/cluster' },
+            { text: 'Users', icon: <PeopleIcon />, link: '/cluster' },
+          ].map((item) => (
+            <ListItem key={item.text} disablePadding>
+              <ListItemButton component={Link} to={item.link} onClick={() => handleItemClick(item.link)}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <IconButton onClick={toggleDrawer(anchor, true)}>
+            <MenuIcon sx={{fontSize:"5vh"}} />
+          </IconButton>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
-
-export default Navigation;
